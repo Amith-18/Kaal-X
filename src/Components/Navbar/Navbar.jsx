53b7ca655aefa61f25f09c8c5c7cebbf8f86f10d
Amith-18 <amithsanil@outlook.com>
2025-01-16 22:51:07 +0530

    added css

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,26 +4,59 @@ import logo from '../../assets/Logo.png'
 
 const Navbar = () => {
   return (
-    <div className='navbar-head'>
-        <div className='logo'>
-            <img src={logo} alt="" />
-        </div>
-        <div className="navbar-menu">
-            <ul className="nav-link">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">Service/Product</a></li>
-                <li><a href="#">Technolgy</a></li>
-                <li><a href="#">Contact Us</a></li>
-            </ul>
-        </div>
-        <div className="login">
-          <button>Log In</button>
-          <button>Sign Up</button>
-        </div>
-        <hr /> 
+    //     <div className='navbar-head'>
+    //       <div className='logo'>
+    //         <img src={logo} alt="" />
+    //       </div>
+    //       <div className="navbar-menu">
+    //         <ul className="nav-link">
+    //           <li><a href="#">Home</a></li>
+    //           <li><a href="#">About Us</a></li>
+    //           <li><a href="#">Service/Product</a></li>
+    //           <li><a href="#">Technolgy</a></li>
+    //           <li><a href="#">Contact Us</a></li>
+    //         </ul>
+    //       </div>
+    //       <div className="login-form">
+    //         <button className='login'>Log In</button>
+    //         <button className='signup'>Sign Up</button>
+    //       </div>
+    //       <div>
+    //        <hr/>
+    //       </div>
+
+    //     </div>
+    //   )
+    // }
+
+
+    <div className="navbar-head">
+      {/* Logo Section */}
+      <div className="logo">
+        <img src={logo} alt="KaalX Logo" />
+      </div>
+
+      {/* Navigation Links */}
+      <div className="navbar-menu">
+        <ul className="nav-link">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Service/Product</a></li>
+          <li><a href="#">Technology</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+      </div>
+
+      {/* Login and Signup Buttons */}
+      <div className="login-form">
+        <button className="login">Log In</button>
+        <button className="signup">Sign Up</button>
+      </div>
+
+      {/* Horizontal Divider */}
+      <hr />
     </div>
-  )
-}
+  );
+};
 
 export default Navbar
